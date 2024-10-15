da9b8403583d3f2878c83237a3f96967913415c8
felipelkpp <168752818+felipelkpp@users.noreply.github.com>
2024-10-15 11:31:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+@import
+url(‘https://fonts.googleapis.com/css2?
+family=Nunito+Sans:ital,opsz,
+wght@0,6..12,200..1000;1,6..12,200..1000&display=swap’);:root {
+    --bg-color: #222831;
+    --primary-color: #DDDDDD;
+    --secundary-color: #F05454;
+    }:root {
+        --font: “Nunito Sans”, sans-serif;body {
+            background-color: var(--bg-color);<head>
+            <meta charset=”UTF-8”>
+            <meta name=”viewport” content=”width=device-width,
+            initial-scale=1.0”>
+            <title>Redes Sociais</title>
+            <link rel=”stylesheet” href=”style.css”>
+            </head>body {
+                background-color: var(--bg-color);
+                color: var(--primary-color);
+                font-family: var(--font);body {
+                    background-color: var(--bg-color);
+                    color: var(--primary-color);
+                    font-family: var(--font);
+                    height: 100vh;
+                    margin: 0;header {
+                        background-color: var(--primary-color);
+                        text-align: center;
+                        }h1 {
+                            font-size: 2rem;
+                            color: var(--bg-color);
+                            font-weight: 700;Aulas do dia 03/10: 
+                                :root {
+                                    --bg-color: #222831;
+                                    --primary-color: #DDDDDD;
+                                    --secundary-color: #F05454;--font: “Nunito Sans”, sans-serif;
+                                    body {
+                                        background-color: var(--bg-color);
+                                        }
